0c0cd399d3a7e945e79c203634cac96c5c17e459
polovykh <polovykh@mail.ru>
2022-05-16 14:16:00 +0700

    Hexlet-test: Use Assert

--- tests/capitalize.test.js
+++ tests/capitalize.test.js
@@ -1,14 +1,16 @@
 // нажмите кнопку run чтобы запустить тесты
 // попробуйте изменять код функции / тестов, запуская проверки заново
-
+import { strict as assert } from 'assert';
 import { capitalize } from '../src/capitalize.js';
 
-if (capitalize('hello') !== 'Hello') {
-  throw new Error('Функция работает неверно!');
-}
+// if (capitalize('hello') !== 'Hello') {
+//   throw new Error('Функция работает неверно!');
+// }
+assert.equal(capitalize('hello'), 'Hello');
 
-if (capitalize('') !== '') {
-  throw new Error('Функция работает неверно!');
-}
+// if (capitalize('') !== '') {
+//   throw new Error('Функция работает неверно!');
+// }
+assert.equal(capitalize(''), '');
 
 console.log('Все тесты пройдены!');
